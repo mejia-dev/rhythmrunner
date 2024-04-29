478b46259ff3aafad3885a56afd98093a9d97ab1
mejia-dev <aaron.mejia8484@gmail.com>
2024-04-29 11:57:20 -0700

    Finalize EnemyObj.ts

---
+++ src/components/EnemyObj.ts
@@ -0,0 +1,43 @@
+import { globalCanvasCtx, globalLevelData, globalRenderX, globalScoreSet, globalPreviousRenderX } from "./GlobalGameLogic";
+
+export default class EnemyObj {
+  id: string;
+  width: number;
+  height: number;
+  moveSpeed: number
+  isAlive: boolean;
+  readyForDeletion: boolean;
+  position: { x: number, y: number }
+
+  constructor(spawnX: number, spawnY: number) {
+    this.id = new Date().toLocaleTimeString();
+    this.width = 50;
+    this.height = 50;
+    this.moveSpeed = 0;
+    this.isAlive = true;
+    this.readyForDeletion = false;
+    this.position = {
+      x: spawnX,
+      y: spawnY
+    }
+  }
+
+  draw() {
+    globalCanvasCtx.fillStyle = "red";
+    globalCanvasCtx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  updateMoveSpeed(): void {
+    const prevLevelX = (globalPreviousRenderX / globalLevelData.length) * globalLevelData[globalLevelData.length - 1].x;
+    const currentLevelX = (globalRenderX / globalLevelData.length) * globalLevelData[globalLevelData.length - 1].x;
+    this.moveSpeed = (currentLevelX - prevLevelX);
+  }
+
+  requestUpdate(deltaTimeMultiplier: number): void {
+    this.updateMoveSpeed();
+    this.position.x -= this.moveSpeed * deltaTimeMultiplier;
+    if (this.isAlive) {
+      this.draw();
+    }
+  }
+}

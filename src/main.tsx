6e3d5afaecf15d57e9cdb1ffd0ae0a0eaef3e076
mejia-dev <aaron.mejia8484@gmail.com>
2024-04-29 15:34:12 -0700

    Make Game render in App

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import Game from './components/Game.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Game />
+    <App />
   </React.StrictMode>,
 )

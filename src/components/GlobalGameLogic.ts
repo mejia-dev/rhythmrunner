6a1151eabf98553df2ce17518db18312001aefcc
mejia-dev <aaron.mejia8484@gmail.com>
2024-04-29 13:48:12 -0700

    Add Canvas initialization

--- src/components/GlobalGameLogic.ts
+++ src/components/GlobalGameLogic.ts
@@ -1,8 +1,5 @@
-let globalAudioBuffer: AudioBuffer;
-let globalAudioContext: AudioContext;
-let globalAudioHTMLElement: HTMLMediaElement;
-let globalAudioIsPlaying: boolean = false;
-let globalCanvas;
+
+let globalCanvas: HTMLCanvasElement;
 export let globalCanvasCtx: CanvasRenderingContext2D;
 let globalEnemyPositionList;
 let globalEnemySpawnedList = [];
@@ -17,8 +14,13 @@ export let globalRenderX: number;
 export let globalPlatformY: number;
 export let globalPreviousRenderX: number;
 
+let globalAudioBuffer: AudioBuffer;
+let globalAudioContext: AudioContext;
+let globalAudioHTMLElement: HTMLMediaElement;
+let globalAudioIsPlaying: boolean = false;
 
-export function handleAudioUpload(event: Event) {
+
+export function handleAudioUpload(event: Event): void {
   let blob: any = window.URL || window.webkitURL;
   const file: any = (event.target as HTMLInputElement)?.files?.[0];
   const fileUrl: string = blob.createObjectURL(file);
@@ -34,7 +36,7 @@ export function handleAudioUpload(event: Event) {
 }
 
 
-function initializeAudioTrack(bufferedAudioArray: ArrayBuffer) {
+function initializeAudioTrack(bufferedAudioArray: ArrayBuffer): void {
   // globalAudioContext = new (window.AudioContext || window.webkitAudioContext)();
   globalAudioContext = new window.AudioContext();
   globalAudioHTMLElement = document.getElementById("audioSource") as HTMLMediaElement;
@@ -44,12 +46,11 @@ function initializeAudioTrack(bufferedAudioArray: ArrayBuffer) {
     globalAudioBuffer = buffer;
     
     initializeAudioControls();
-    // createLevelData();
-    // startCanvas();
-  })
+    initializeCanvas();
+  });
 }
 
-function initializeAudioControls() {
+function initializeAudioControls(): void {
   const playButton: HTMLButtonElement = document.getElementById("playButton") as HTMLButtonElement;
   playButton.addEventListener("click", () => {
     if (globalAudioContext.state === "suspended") {
@@ -68,4 +69,43 @@ function initializeAudioControls() {
   },
     false,
   );
+}
+
+function initializeCanvas(): void {
+  globalCanvas = document.getElementById("visualizer") as HTMLCanvasElement;
+  globalCanvasCtx = globalCanvas.getContext("2d") as CanvasRenderingContext2D;
+  createLevelData();
+  startCanvas();
+}
+
+function createLevelData(): void {
+  const audioData = globalAudioBuffer.getChannelData(0);
+  const samplesCount = audioData.length;
+  const levelWidth = globalAudioBuffer.duration * 1000;
+  const levelHeight = globalCanvas.height;
+
+  globalPlatformY = (globalCanvas.height / 3) * 2
+  globalLevelData = [];
+  globalEnemyPositionList = [];
+
+  for (let i = 0; i < samplesCount; i += Math.floor(samplesCount / levelWidth)) {
+    const sample = Math.abs(audioData[i]);
+    const posX = (i / samplesCount) * levelWidth;
+    const posY = Math.floor(sample * levelHeight) / 2;
+    if (posY > 200) {
+      // 50 is currently arbitrary representation of final enemy height.
+      globalEnemyPositionList.push({ x: posX, y: globalPlatformY - 50 });
+    }
+    globalLevelData.push({ x: posX, y: levelHeight - posY });
+  }
+}
+
+function startCanvas(): void {
+  globalRenderX = 0;
+  globalCanvasCtx.fillStyle = "black";
+  globalCanvasCtx.fillRect(0, 0, globalCanvas.width, globalCanvas.height);
+  // player1.position.x = globalCanvas.width / 2 - 50;
+  globalEnemySpawnInterval = setInterval(() => {
+    globalEnemyTimer++;
+  }, 1000);
 }
